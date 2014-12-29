0feb3c36f924d5096ea4d1f16f33388004723107
zeesuulan <zeesuulan@gmail.com>
2014-12-30 01:32:29 +0800

    first init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yizhifuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yizhifuApp
+ */
+angular.module('yizhifuApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
