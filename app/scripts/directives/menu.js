9dc15dbfcd16f83b91868b99ef74c80360df7d90
zeesuulan <zeesuulan@gmail.com>
2015-01-06 23:16:29 +0800

    完成菜单

--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -7,12 +7,41 @@
  * # yMenu
  */
 angular.module('yizhifuApp')
-  .directive('yMenu', function () {
-    return {
-      templateUrl: 'views/part/menu.html',
-      restrict: 'A',
-      replace: true,
-      link: function postLink(scope, element, attrs) {
-      }
-    };
-  });
+	.directive('yMenu', function() {
+		return {
+			templateUrl: 'views/part/menu.html',
+			restrict: 'A',
+			replace: true,
+			link: function postLink(scope, element, attrs) {
+
+
+				scope.isShowUserManagement = false
+				scope.isShowSettleManagement = false
+
+				var toggle = {
+					"userManagement": function() {
+						scope.isShowUserManagement = !scope.isShowUserManagement
+					},
+					"settleManagement": function() {
+						scope.isShowSettleManagement = !scope.isShowSettleManagement
+					},
+					"logQuery": function() {
+						scope.isShowLogQuery = !scope.isShowLogQuery
+					}
+				}
+
+				scope.open = function(type) {
+					if (!scope.province) {
+						alert("请选择省平台!")
+						return
+					}
+					if (typeof(toggle[type]) == 'function') {
+						toggle[type].call()
+					}
+				}
+
+
+
+			}
+		};
+	});
